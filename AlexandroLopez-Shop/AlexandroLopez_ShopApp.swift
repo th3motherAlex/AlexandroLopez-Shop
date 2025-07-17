0e549f49dbd00c1ea9e790941a3b16800d5e1b13
Carlos Alexandro <pokebol7771@gmail.com>
2025-07-16 20:19:26 -0600

    Initial Commit

---
+++ AlexandroLopez-Shop/AlexandroLopez_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlexandroLopez_ShopApp.swift
+//  AlexandroLopez-Shop
+//
+//  Created by Carlos López García on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AlexandroLopez_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
